c8c2785aa70306de1ccb263c46f4143d8cb576fe
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-09 16:14:03 -0400

    stuff

--- components/intro.js
+++ components/intro.js
@@ -1,14 +1,21 @@
-import { CMS_NAME, RAND_EMOJI } from '../lib/constants'
+import { CMS_NAME } from '../lib/constants'
 
 export default function Intro() {
   return (
+    <>
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-  {RAND_EMOJI()}  Hackagotchi!
+        <strike>MLNMA</strike>
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        The cutest game available for <a className="underline hover:text-accent-2" href="https://hackclub.com">Hack Club</a>
+        A blog about machine learning and math.
       </h4>
     </section>
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <p className="">
+        <strike>MLNMA</strike>
+      </p>
+    </section>
+    </>
   )
 }
