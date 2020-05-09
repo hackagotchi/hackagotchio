5b237a60ea597a218cf24cd136442f36cb66d919
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-09 16:12:26 -0400

    fotter updates

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,6 @@
 import Container from './container'
 import { EXAMPLE_PATH } from '../lib/constants'
+import { FiGithub, FiBookOpen } from 'react-icons/fi'
 
 export default function Footer() {
   return (
@@ -11,10 +12,16 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="/"
+              href="https://rishi.js.org"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              ~/
+              rishi
+            </a>
+            <a
+              href={`https://github.com/rishiosaur/mlnma`}
+              className="mx-3 font-bold hover:underline text-black"
+            >
+              github
             </a>
           </div>
         </div>
