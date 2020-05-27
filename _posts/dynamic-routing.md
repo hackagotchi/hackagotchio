231875e89af337dee881bc0a3fee561cc76e4297
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-26 23:13:48 -0400

    final changes

--- _posts/dynamic-routing.md
+++ _posts/dynamic-routing.md
@@ -11,6 +11,7 @@ ogImage:
 categories:
   - 'aefawewef'
   - 'awefnawe'
+emoji: '🔆'
 ---
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Praesent elementum facilisis leo vel fringilla. Congue mauris rhoncus aenean vel. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.
--- components/footer.js
+++ components/footer.js
@@ -12,16 +12,16 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://rishi.js.org"
+              href="https://hackclub.com"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              rishi
+              hack.
             </a>
             <a
-              href={`https://github.com/rishiosaur/mlnma`}
+              href={`https://github.com/hackagotchi`}
               className="mx-3 font-bold hover:underline text-black"
             >
-              github
+              github.
             </a>
           </div>
         </div>
