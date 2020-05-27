0b3ba211750f7180ccae7bd259fddecfb7a5d3e7
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-26 23:13:39 -0400

    redo naming

--- components/intro.js
+++ components/intro.js
@@ -1,20 +1,18 @@
 import { CMS_NAME } from '../lib/constants'
 
 export default function Intro() {
+
   return (
     <>
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        <strike>MLNMA</strike>
+        Hackagotchi
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A blog about machine learning and math.
+        The cutest Slack Bot. Built by and for Hack Club.
       </h4>
     </section>
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-    <p className="">
-        <strike>MLNMA</strike>
-      </p>
     </section>
     </>
   )
