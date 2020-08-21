2a295d9432f63f521311ab945e9d0841ca283930
Owen Salter <owen@devosmium.xyz>
2020-08-21 16:33:18 -0400

    remove cruft

--- components/date-formater.js
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-export default function DateFormater({ dateString, emoji }) {
-  const date = parseISO(dateString)
-  return <p><time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time> - Feeling {emoji}</p>
-}
